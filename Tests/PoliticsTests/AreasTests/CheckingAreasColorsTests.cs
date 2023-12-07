e863ec71af2d97a9d021673b520ad4f1fbcf0bf8
insaniaOfficialis <insania_officialis@vk.com>
2023-12-07 17:09:25 +0300

    Добавлены тесты проверки цвета областей

---
+++ Tests/PoliticsTests/AreasTests/CheckingAreasColorsTests.cs
@@ -0,0 +1,65 @@
+﻿using Services;
+using Services.Politics.Areas.CheckingAreasColors;
+
+namespace Tests.PoliticsTests.AreasTests;
+
+/// <summary>
+/// Тесты проверки цвета областей
+/// </summary>
+public class CheckingAreasColorsTests : BaseTest
+{
+    /// <summary>
+    /// Тесты проверки цвета областей
+    /// </summary>
+    public CheckingAreasColorsTests() : base()
+    {
+    }
+
+    /// <summary>
+    /// Тест на проверку успешности
+    /// </summary>
+    [Fact]
+    public async void Success()
+    {
+        //Создаём новый экземпляр сервиса
+        CheckingAreasColors service = new(_repository);
+
+        //Получаем результат
+        var result = await service.Handler("#000000");
+
+        //Проверяем результат
+        Assert.True(result.Success);
+    }
+
+    /// <summary>
+    /// Тест на проверку пустого запроса
+    /// </summary>
+    [Fact]
+    public async void EmptyRequest()
+    {
+        //Создаём новый экземпляр сервиса
+        CheckingAreasColors service = new(_repository);
+
+        //Получаем результат
+        var result = await service.Handler(null);
+
+        //Проверяем результат
+        Assert.Equal(Errors.EmptyRequest, result.Error?.Message);
+    }
+
+    /// <summary>
+    /// Тест на проверку занятого цвета
+    /// </summary>
+    [Fact]
+    public async void BusyColor()
+    {
+        //Создаём новый экземпляр сервиса
+        CheckingAreasColors service = new(_repository);
+
+        //Получаем результат
+        var result = await service.Handler("#828CD8");
+
+        //Проверяем результат
+        Assert.Equal(Errors.BusyColor, result.Error?.Message);
+    }
+}
